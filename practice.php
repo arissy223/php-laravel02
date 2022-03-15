0cf209f6016c8acfa2d12524daefd271e213b008
arisa <arisatsutsumi223@gmail.com>
2022-03-15 12:54:17 +0000

    php02

---
+++ practice.php
@@ -0,0 +1,19 @@
+<?php 
+echo 'hello php!';
+$a = 3;
+$b = 7;
+echo $a + $b;
+echo $a;
+$array_month = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+echo $array_month[7];
+$hello = "Hello,";
+$name = "Arisa";
+$world = "s world!";
+echo $hello . "Arisa" . "'s world!";
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
+
+
+
+
